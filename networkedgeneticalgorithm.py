ac0163ca4e628a0ac6ff1b9b6fc006e2e3cff8cd
Joel Forster <joel@eduroam-int-dhcp-97-209-3.ucl.ac.uk>
2017-07-31 18:48:18 +0100

    shifted the ga into its own class, added lambdas for control

---
+++ networkedgeneticalgorithm.py
@@ -0,0 +1,96 @@
+import array
+import random
+import math
+
+import itertools
+import operator
+
+import numpy
+from customMap import customMap
+import networks
+
+from deap import algorithms
+from deap import base
+from deap import creator
+from deap import tools
+
+import joblib
+
+from joblib import Parallel, delayed, parallel_backend
+from distributed.joblib import DaskDistributedBackend
+
+
+class NetworkedGeneticAlgorithm:
+
+    def __init__(self,creator,genomeSize,evaluate,mate,mut,sel,net,subroutine,islePop):
+
+        
+        self.toolbox = base.Toolbox()
+
+        # Attribute generator
+        self.toolbox.register("attr_bool", random.randint, 0, 1)
+
+        # Structure initializers
+        self.toolbox.register("individual", tools.initRepeat, creator.Individual, self.toolbox.attr_bool, genomeSize)
+        self.toolbox.register("population", tools.initRepeat, list, self.toolbox.individual)
+
+        self.toolbox.register("evaluate", evaluate)
+        self.toolbox.register("mate", mate)
+        self.toolbox.register("mutate", mut)
+        self.toolbox.register("select", sel)
+        self.toolbox.register("map", map)
+        self.net = net
+        self.subroutine = subroutine
+        self.islePop = islePop
+
+    def genMetrics(self,gen,island,logbook):
+        return (gen,island, numpy.max([d['max'] for d in logbook]))
+
+    def getTotalFitness(self,pop):
+        return numpy.sum([p.fitness.values[-1] for p in pop])
+
+    def accumulateMetrics(self,lis):
+        for k, g in itertools.groupby(lis, key=operator.itemgetter(0)):
+            lis = [y for x in g for y in x[1:]]
+            fits = [t[2] for t in lis]
+            yield (k[0],numpy.mean(fits),numpy.std(fits),numpy.max(fits),numpy.min(fits))
+
+    def migration(self,islands):
+        network = self.net
+        rFitness = 1.0/numpy.sum(map(self.getTotalFitness,islands))
+        s = random.uniform(0,1)
+        t = 0.0
+        for i in range(len(islands)):
+            isle = islands[i]
+            for ind in isle:
+                f = ind.fitness.values[-1]*rFitness
+                if f + t >= s:
+                    edges = network.edges(network.nodes()[i])
+                    if len(edges)>0:
+                        edge = random.choice(edges)[-1]
+                        dest = network.nodes().index(edge)
+                        #print("migrating " + str(ind) + " from " + str(i)  + " to " + str(dest))
+                        islands[dest][random.randint(0,len(islands[dest])-1)] = ind
+                    return
+                else:
+                    t+=f
+
+    def algorithm(self,pop):
+    	return self.subroutine(pop,self.toolbox)
+        
+
+    def run(self,ngen,freq):
+        metrics = []
+        self.islands = [self.toolbox.population(n=self.islePop) for i in range(len(self.net))]
+        for i in range(0, ngen, freq):
+            results = self.toolbox.map(self.algorithm, self.islands)
+            self.islands = [pop for pop, logbook in results]
+            metrics.append(map(self.genMetrics,[i+freq]*len(results),[n for n in range(len(results))], [logbook for pop, logbook in results]))
+            self.migration(self.islands)
+        metrics = list(self.accumulateMetrics(metrics))
+        return self.islands, metrics
+
+
+
+
+
--- run.py
+++ run.py
@@ -1,6 +1,11 @@
 import array
 import random
 import math
+import csv
+import argparse
+
+import itertools
+import operator
 
 import numpy
 from customMap import customMap
@@ -16,6 +21,16 @@ import joblib
 from joblib import Parallel, delayed, parallel_backend
 from distributed.joblib import DaskDistributedBackend
 
+import networkedgeneticalgorithm as nga
+
+NSPOKES = 2
+NISLES = 11
+ISLESIZE = 5
+CXPB=0.5
+MUTPB=0.2
+NGEN, FREQ = 100, 2
+VERBOSE=False
+mStar = networks.createMegaStar(NSPOKES,int(math.ceil((NISLES-3)*0.25)),int(math.floor((NISLES-3)*0.25)))
 
 def cxTwoPointCopy(ind1, ind2):
     size = len(ind1)
@@ -34,84 +49,50 @@ def cxTwoPointCopy(ind1, ind2):
 def evalOneMax(individual):
     return sum(individual),
 
-creator.create("FitnessMax", base.Fitness, weights=(1.0,))
-creator.create("Individual", numpy.ndarray, fitness=creator.FitnessMax)
+def selTournament(pop,k):
+    return tools.selTournament(pop,k,3)
 
-toolbox = base.Toolbox()
+def mutFlipBit(individual):
+    return tools.mutFlipBit(individual,0.05)
 
-# Attribute generator
-toolbox.register("attr_bool", random.randint, 0, 1)
 
-# Structure initializers
-toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_bool, 100)
-toolbox.register("population", tools.initRepeat, list, toolbox.individual)
-
-toolbox.register("evaluate", evalOneMax)
-toolbox.register("mate", cxTwoPointCopy)
-toolbox.register("mutate", tools.mutFlipBit, indpb=0.05)
-toolbox.register("select", tools.selTournament, tournsize=3)
-toolbox.register("map", map)
-
-hof = tools.HallOfFame(1, similar=numpy.array_equal)
-stats = tools.Statistics(lambda ind: ind.fitness.values)
-stats.register("avg", numpy.mean)
-stats.register("std", numpy.std)
-stats.register("min", numpy.min)
-stats.register("max", numpy.max)
-
-NSPOKES = 2
-NISLES = 11
-ISLESIZE = 5
-CXPB=0.5
-MUTPB=0.2
-NGEN, FREQ = 100, 5
-VERBOSE=False
-mStar = networks.createMegaStar(NSPOKES,int(math.ceil((NISLES-3)*0.25)),int(math.floor((NISLES-3)*0.25)))
-
-def algorithm(pop):
+def algorithm(pop,toolbox):
+    hof = tools.HallOfFame(1, similar=numpy.array_equal)
+    stats = tools.Statistics(lambda ind: ind.fitness.values)
+    stats.register("avg", numpy.mean)
+    stats.register("std", numpy.std)
+    stats.register("min", numpy.min)
+    stats.register("max", numpy.max)
     return algorithms.eaSimple(pop,toolbox=toolbox,
         cxpb=CXPB, mutpb=MUTPB, ngen=FREQ,verbose=VERBOSE,stats=stats,halloffame=hof)
 
-def saveMetrics(n,logbook):
-    print(n, numpy.max([d['max'] for d in logbook]))
-
-def getTotalFitness(pop):
-    return numpy.sum([p.fitness.values[-1] for p in pop])
-
-def migration(islands,network):
-    rFitness = 1.0/numpy.sum(map(getTotalFitness,islands))
-    s = random.uniform(0,1)
-    t = 0.0
-    for i in range(len(islands)):
-        isle = islands[i]
-        for ind in isle:
-            f = ind.fitness.values[-1]*rFitness
-            if f + t >= s:
-                edges = network.edges(network.nodes()[i])
-                if len(edges)>0:
-                    edge = random.choice(edges)[-1]
-                    dest = network.nodes().index(edge)
-                    islands[dest][random.randint(0,len(islands[dest])-1)] = ind
-                return
-            else:
-                t+=f
+def saveMetrics(lis):
+    with open('ur file.csv','wb') as out:
+        csv_out=csv.writer(out)
+        csv_out.writerow(['gen','avg','std','min','max'])
+        for row in lis:
+            csv_out.writerow(row)
 
 
 def main():
+
+    creator.create("FitnessMax", base.Fitness, weights=(1.0,))
+    creator.create("Individual", numpy.ndarray, fitness=creator.FitnessMax)
+
     random.seed(64)
-    islands = [toolbox.population(n=ISLESIZE) for i in range(NISLES)]
+    ga = nga.NetworkedGeneticAlgorithm(creator,
+        100,
+        evalOneMax,
+        cxTwoPointCopy,
+        mutFlipBit,
+        selTournament,
+        mStar,
+        algorithm,
+        10)
+    results = ga.run(NGEN,FREQ)
+    saveMetrics(results[-1])
 
-    # Unregister unpicklable methods before sending the toolbox.
-    toolbox.unregister("attr_bool")
-    toolbox.unregister("individual")
-    toolbox.unregister("population")
 
-    for i in range(0, NGEN, FREQ):
-        results = toolbox.map(algorithm, islands)
-        islands = [pop for pop, logbook in results]
-        map(saveMetrics,[n + i for n in range(len(results))], [logbook for pop, logbook in results])
-        migration(islands,mStar)
-    return islands
 
 if __name__ == "__main__":
     main()
