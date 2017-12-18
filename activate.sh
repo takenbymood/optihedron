f0a5ce127d805b98fa4862ae0204c8d5194032ed
forsterjoel@gmail.com <forsterjoel@gmail.com>
2017-12-18 22:49:15 +0000

    TK fixes

--- activate.sh
+++ activate.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+
+TK_LIBRARY=/usr/lib/python2.7/lib-tk:/usr/lib/python2.7/site-packages/PIL:/usr/lib
+TKPATH=/usr/lib/python2.7/lib-tk:/usr/lib/python2.7/site-packages/PIL:/usr/lib 
+TCL_LIBRARY=/usr/lib 
+export TCL_LIBRARY TK_LIBRARY TKPATH
 export DYLD_FALLBACK_LIBRARY_PATH=$(pwd)/lammps/src:$DYLD_FALLBACK_LIBRARY_PATH
 export TMPDIR=~/tmp
 
