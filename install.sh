a2adb5e9336360189a0169471fc06c7bd1f4a5fd
Joel Forster <forsterjoel@gmail.com>
2017-12-19 15:55:21 +0000

    removed gpu dependency in install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 #this script requires wget, boost, nvcc, python 2.7 and pip
 #OS-X has trouble with the gpu package of lammps, install XCode8 (not 9) and CUDA
 
+#commented out lines allow for the building of a gpu accelerated version. I will clean this process up at some point...
+
 
 wdir=$(pwd)
 CUDA_HOME=/usr/local/cuda
@@ -16,11 +18,11 @@ if [ ! -f ${LAMMPSDIR}/src/liblammps.so ]; then
 	cd src
 	make clean-all
 	make no-all
-	make yes-rigid yes-molecule yes-python yes-opt
-	cd ${LAMMPSDIR}/lib/gpu && make -f Makefile.mpi CUDA_LIB="-L${CUDA_HOME}/lib"
+	make yes-rigid yes-molecule yes-python yes-opt #yes-gpu
+	#cd ${LAMMPSDIR}/lib/gpu && make -f Makefile.mpi CUDA_LIB="-L${CUDA_HOME}/lib"
 	cd ${LAMMPSDIR}/src
-	make mpi LMP_INC="-DLAMMPS_PNG -DLAMMPS_JPEG -DLAMMPS_FFMPEG -DLAMMPS_EXCEPTIONS" JPG_LIB="-lpng -ljpeg" gpu_SYSPATH="-L${CUDA_HOME}/lib"
-	make mpi mode=shlib LMP_INC="-DLAMMPS_PNG -DLAMMPS_JPEG -DLAMMPS_FFMPEG -DLAMMPS_EXCEPTIONS" JPG_LIB="-lpng -ljpeg" gpu_SYSPATH="-L${CUDA_HOME}/lib"
+	make mpi #LMP_INC="-DLAMMPS_PNG -DLAMMPS_JPEG -DLAMMPS_FFMPEG -DLAMMPS_EXCEPTIONS" JPG_LIB="-lpng -ljpeg" gpu_SYSPATH="-L${CUDA_HOME}/lib"
+	make mpi mode=shlib #LMP_INC="-DLAMMPS_PNG -DLAMMPS_JPEG -DLAMMPS_FFMPEG -DLAMMPS_EXCEPTIONS" JPG_LIB="-lpng -ljpeg" gpu_SYSPATH="-L${CUDA_HOME}/lib"
 fi
 if [ ! -d ${wdir}/venv ]; then
 	pip install virtualenv
