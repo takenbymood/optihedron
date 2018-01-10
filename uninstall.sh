853104cf027821fd643032c57ee1c8d2d1d755db
Joel Forster <forsterjoel@gmail.com>
2018-01-10 15:22:06 +0000

    added changes to install and uninstall scripts to enable 3D code in the main scripts

---
+++ uninstall.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+WDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+while test $# -gt 0; do
+        case "$1" in
+                -h|--help)
+						echo "EMERGENT OPTIMISER UNINSTALLER"
+						echo " "
+                        echo "A script for uninstalling and cleaning the emergent optimiser package."
+                        echo " "
+                        echo "options:"
+                        echo "-h, --help                show brief help"
+                        echo "-r, --remove              remove ALL files and folders related to installation (rather than just removing binaries and libraries)"
+                        exit 0
+                        ;;
+                -r|--remove)
+						rm -rf ${WDIR}/lammps
+                        ;;
+                *)
+                        break
+                        ;;
+        esac
+done
+
+
+cd lammps
+LAMMPSDIR=$(pwd)
+if [ -f ${LAMMPSDIR}/src/liblammps.so ]; then
+	rm ${LAMMPSDIR}/src/liblammps.so
+	rm ${LAMMPSDIR}/src/lmp_mpi
+	rm ${LAMMPSDIR}/src/lmp_serial
+	rm ${LAMMPSDIR}/src/lammps
+	cd src
+	make clean-all
+	make no-all
+fi
+cd ${WDIR}
+if [ -d ${WDIR}/venv ]; then
+	rm -rf ${WDIR}/venv
+fi
