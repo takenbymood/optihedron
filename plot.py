10fb17634c3aa4d079ad9a5b96c589ce3d82566f
potato_farmer <wildtangles@gmail.com>
2018-02-08 02:39:47 +0000

    general cleanup
    removed unused variables. added in and commented out bits from original
    version that were unused to not lose old work.

--- plot.py
+++ plot.py
@@ -38,14 +38,10 @@ def datafromtxt(filepath):
     return np.genfromtxt(filepath, delimiter=',', skip_header=1,
                      skip_footer=0, names=['STD','MAX','AVG','GEN','MIN'])
 
-# example data
 data = []
-fname = []
 data.append(datafromtxt(args.filepath))
-fname.append(os.path.basename(args.filepath))
 for afilepath in args.addfilepath:
-    data.append(datafromtxt(afilepath))
-    fname.append(os.path.basename(afilepath))
+    data.append(datafromtxt(afilepath))    
 
                      # These are the "Tableau 20" colors as RGB.    
 tableau20 = [(31, 119, 180), (174, 199, 232), (255, 127, 14), (255, 187, 120),    
@@ -59,23 +55,24 @@ for i in range(len(tableau20)):
     r, g, b = tableau20[i]    
     tableau20[i] = (r / 255., g / 255., b / 255.)    
 
+
+# x = data['GEN']
+# yorig = data['AVG']
+# y = yorig
+# y2 = data['MAX']
+# err = data['STD']
+# #errN = map(math.sqrt,data['NV'])
+# stdErr = err/10
+# errPerc = (stdErr/yorig)
+# yerr = y*errPerc
 x = []
-yorig = []
 y = []
 y2 = []
-err = []
-#errN = map(math.sqrt,data['NV'])
-stdErr = []
-errPerc = []
 yerr = []
 for datum in data:
-    x.append(datum['GEN'])
-    yorig.append(datum['AVG'])
+    x.append(datum['GEN'])    
     y.append(datum['AVG'])
-    y2.append(datum['MAX'])
-    err.append(datum['STD'])
-    stdErr.append(datum['STD']/10)
-    errPerc.append((datum['STD']/10)/datum['AVG'])
+    y2.append(datum['MAX'])            
     yerr.append(datum['AVG']*((datum['STD']/10)/datum['AVG']))
 
 fig = plt.figure(figsize=(args.figurex,args.figurey))
@@ -90,22 +87,24 @@ ax1.set_ylabel('Fitness')
 
 #plt.fill_between(x, y-yerr,y+yerr, color="#3F5D7D")  
 for n in range(0,100,2):
-    plt.plot([-1,np.max([np.max(i) for i in x])+1], [n,n], "--", lw=0.5, color="black", alpha=0.3)  
+    plt.plot([-1, np.max([np.max(i) for i in x])+1], [n,n], "--", lw=0.5, color="black", alpha=0.3)  
     
 plt.tick_params(axis="both", which="both", bottom="off", top="off",labelbottom="on", left="off", right="off", labelleft="on") 
 
-for x_i, y_i, y2_i, yerr_i, fname_i, color_i in zip(x, y, y2, yerr, fname, tableau20[::2]):
-    ax1.plot(x_i,y2_i,lw=1.5,label='Maximum {}'.format(fname_i),color=color_i)
-    ax1.errorbar(x_i,y_i, yerr=yerr_i, markersize='3.5',capsize=2.5, fmt='o-', label='Average {}'.format(fname_i), color=color_i)    
+for x_i, y_i, y2_i, yerr_i, data_i, color_i in zip(x, y, y2, yerr, data, tableau20[::2]):
+    fname = os.path.basename(data_i)
+    ax1.plot(x_i, y2_i, color=color_i, lw=1.5, label='Maximum {}'.format(fname))
+    ax1.errorbar(x_i, y_i, yerr=yerr_i, color=color_i, markersize='3.5', capsize=2.5, fmt='o-', label='Average {}'.format(fname))    
 
+#plt.legend(bbox_to_anchor=(0.675, 0.2), loc=2, borderaxespad=0.)
 plt.legend()
 
 
 plt.ylim(tworound(np.min([np.min(i) for i in y])-3),tworound(np.max([np.max(i) for i in y2])+2))
 
 #plt.ylim(20,60)
-
 plt.xlim(0,(np.max([np.max(i) for i in x]))+1)
 
 plt.draw()
 plt.savefig(outfile)
+#plt.show()
