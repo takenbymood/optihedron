5ea635e9bd5f3aa507ea8b89374fa28028291a6f
Eugen Rožić <erozic@zoho.eu>
2018-02-26 16:54:44 +0000

    Scripts now avoid problems with absolute paths (e.g. spaces in paths, not being called from the containing directory etc.); command 'make install-python' not called because unneccessary and causes artefacts on the system outside the installing directory; added 'python-dev' as a required package; removed trying to install virtualenv (user's responsibility, and will almost certainly not go through since permissions needed)

--- activate.sh
+++ activate.sh
@@ -6,8 +6,8 @@ TK_LIBRARY=/usr/lib/python2.7/lib-tk:/usr/lib/python2.7/site-packages/PIL:/usr/l
 TKPATH=/usr/lib/python2.7/lib-tk:/usr/lib/python2.7/site-packages/PIL:/usr/lib 
 TCL_LIBRARY=/usr/lib 
 export TCL_LIBRARY TK_LIBRARY TKPATH
-export DYLD_FALLBACK_LIBRARY_PATH=$(pwd)/lammps/src:$DYLD_FALLBACK_LIBRARY_PATH
+export DYLD_FALLBACK_LIBRARY_PATH="$WDIR/lammps/src:$DYLD_FALLBACK_LIBRARY_PATH"
 export TMPDIR=~/tmp
 
 
-source ${WDIR}/venv/bin/activate
+source "$WDIR/venv/bin/activate"
