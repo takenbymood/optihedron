2bae2906147f7da1994ae27cf14068e477a66507
potato_farmer <wildtangles@gmail.com>
2018-04-04 01:47:10 +0100

    added sklearn to installed libs for tSNE

--- pipinstall.sh
+++ pipinstall.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pip install numpy deap joblib networkx pydotplus matplotlib distributed pathos sqlalchemy mpi4py
+pip install numpy deap joblib networkx pydotplus matplotlib distributed pathos sqlalchemy mpi4py sklearn
