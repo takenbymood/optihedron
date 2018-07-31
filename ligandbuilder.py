a57125c6d477081638a29393f2f897d1519abced
Joel Forster <forsterjoel@gmail.com>
2018-07-31 15:36:04 +0100

    moved everything into sensible files

--- ligandbuilder.py
+++ ligandbuilder.py
@@ -2,9 +2,7 @@ import networkx as nx
 import random
 import numpy as np
 import nanoparticle
-import run
 import os
-
 
 def gArcDist(Ang1, Ang2, rad=4):
     #Ang = (PolarAng,AziAng)
@@ -18,17 +16,14 @@ def buildNanoParticleFromNetwork(G,weight,radius=4,sig=1):
         particle.ligands.append(nanoparticle.Ligand(weight,sig,radius,w['pol'],w['azi']))
     return particle
 
-def createRandomLigandNetwork(n):
-    SWG = nx.complete_graph(n)
-    minWeight = 100
-    maxWeight = 0
+def createLigandNetworkFromGraph(G):
     azipol = []
-    for (n,w) in SWG.nodes(data=True):
+    for (n,w) in G.nodes(data=True):
         delta = 0
         azi = 0
         pol = 0
         attempts = 0
-        while delta < 1.65 and attempts < 100:
+        while delta < 1.65 and attempts < 1000:
             azi = random.uniform(0,6.2831)
             pol = random.uniform(0,3.141)
             minDelta = 100
@@ -38,29 +33,27 @@ def createRandomLigandNetwork(n):
                     minDelta = d
             delta = minDelta
             attempts += 1
+        if attempts >= 100:
+            break
         azipol.append((pol,azi))
         w['azi'] = azi
         w['pol'] = pol
         
     if attempts >= 100:
-            return createRandomLigandNetwork(n)
+        return createLigandNetworkFromGraph(G)
+    else:
+        for (u,v,w) in G.edges(data=True):
+            unode = G.nodes(data=True)[u]
+            vnode = G.nodes(data=True)[v]
+            w['weight'] = 1.0/gArcDist((unode['pol'], unode['azi']),(vnode['pol'], vnode['azi']))
+    return G
 
-    for (u,v,w) in SWG.edges(data=True):
-        unode = SWG.nodes(data=True)[u]
-        vnode = SWG.nodes(data=True)[v]
-        w['weight'] = 1.0/gArcDist((unode['pol'], unode['azi']),(vnode['pol'], vnode['azi']))
+def createRandomLigandNetwork(n):
+    G = nx.complete_graph(n)
+    G = createLigandNetworkFromGraph(G)
+    return G
+
+def createSmallWorldLigandNetwork(n,p):
+    SWG = nx.watts_strogatz_graph(n,n-1,p, seed=None)
+    G = createLigandNetworkFromGraph(SWG)
     return SWG
-
-def runEpsSweep(minNodes,maxNodes,totalEps):
-    metrics = []
-    for nodes in range(minNodes,maxNodes+1):
-        weight = float(totalEps)/float(nodes)
-        net = createRandomLigandNetwork(nodes)
-        particle = buildNanoParticleFromNetwork(net,weight)
-        f = run.evaluateParticle(particle,"sweep_"+str(nodes))
-        metrics.append((nodes,f))
-        with open(os.path.join('epssweep_'+str(totalEps)+'.csv'), 'a') as file_:
-            file_.write(str((nodes,f)).replace('(','').replace(')','').replace(' ','')[:-2]+"\n")
-
-
-runEpsSweep(20,72,450)
