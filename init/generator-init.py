ae5d0aeceeb223d169c55f88cc708eff5f7a1a87
WildTangles <WildTangles@gmail.com>
2018-08-07 15:17:40 +0530

    generalised init generators
    moved fixed affinity genome generator into its own seperate generators lib

--- init/generator-init.py
+++ init/generator-init.py
@@ -1,22 +1,10 @@
 import json
+import generators
 import numpy as np
 
 epstotal = 150
 numligandtiles = 72
 epsmin = 14
-
-def guessGenome(epstotal,epsmin):    
-    numLigandsExact = float(epstotal)/float(epsmin)
-    numLigandsApprox = int(numLigandsExact)
-    numLigandsRemainder = numLigandsExact - numLigandsApprox
-    numLigandsInit = numLigandsApprox + np.random.choice([0,1],p=[1.0-numLigandsRemainder,numLigandsRemainder])
-
-    cleanGenome = [0] * numligandtiles    
-    replacementPos = np.random.choice(len(cleanGenome), size=numLigandsInit, replace=False)
-    for i in replacementPos:
-        cleanGenome[i] = 1
-
-    return cleanGenome
 
 initPop = []
 pop = 78
@@ -25,7 +13,7 @@ demes = 2
 for d in range(demes):
     popTmp = []
     for p in range(pop):
-        popTmp.append(guessGenome(epstotal, epsmin))
+        popTmp.append(generators.fixedAffinityGenomes(epstotal, epsmin, numligandtiles))        
     initPop.append(popTmp)
 
 
