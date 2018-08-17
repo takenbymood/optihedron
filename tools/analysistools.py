5d40539eab723d318078c7ee8e10e78d9953afbb
forsterjoel@gmail.com <forsterjoel@gmail.com>
2018-08-17 19:02:01 +0100

    double heatmap

--- tools/analysistools.py
+++ tools/analysistools.py
@@ -144,6 +144,14 @@ def gnmRandomWeightedGraph(nodes,edges,weight):
         
     return G
 
+def transparent_cmap(cmap, N=255):
+    "Copy colormap and set alpha values"
+
+    mycmap = cmap
+    mycmap._init()
+    mycmap._lut[:,-1] = np.linspace(0, 0.8, N+4)
+    return mycmap
+
 def smallWorldNess(G):
     gnmwG = gnmw(G)
     RG = gnmRandomWeightedGraph(gnmwG[0],gnmwG[1],gnmwG[2])
