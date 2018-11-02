b6b27c722b4bd9473cedbc5b391603781a2e4587
Joel Forster <forsterjoel@gmail.com>
2018-11-02 16:16:24 +0000

    added better piplist

--- pipinstall.sh
+++ pipinstall.sh
@@ -1,3 +1,5 @@
-#!/bin/bash
+# #!/bin/bash
 
-pip install numpy deap joblib networkx pydotplus matplotlib distributed pathos sqlalchemy mpi4py sklearn holoviews bokeh pandas tornado==4.5.3 ipykernel==4.8.2 seaborn k3d
+# pip install numpy deap joblib networkx pydotplus matplotlib distributed pathos sqlalchemy mpi4py sklearn holoviews bokeh pandas tornado==4.5.3 ipykernel==4.8.2 seaborn k3d
+
+pip install -r ./requirements.txt
