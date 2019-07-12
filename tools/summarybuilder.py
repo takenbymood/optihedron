2f5d6d3794acfc6ff48b60180200a89fa3ed7e51
Joel Forster <forsterjoel@gmail.com>
2019-07-12 22:29:04 +0100

    finished particle coverage measurements

--- tools/summarybuilder.py
+++ tools/summarybuilder.py
@@ -30,30 +30,8 @@ with open(os.path.join(outPath,'trajectories.pickle'), 'wb') as f:
 
 df = pd.DataFrame()
 data = []
-bmsd = []
-nmsd = []
 for k,v in s.iteritems():
-    if (len(bmsd) > 0 and len(nmsd) > 0) and (len(v['msd']) != len(bmsd) or len(v['msd']) != len(nmsd)):
-        continue
     data.append((k,v['density'],v['clustering'],v['bt'],'budding' if v['bt'] > 0.0 else 'non budding'))
-    try:
-        if v['bt'] > 0.0:
-            bmsd = [bmsd[i]+t for i,t in enumerate(v['msd'])] if len(bmsd) > 0 else [t for i,t in enumerate(v['msd'])]
-        else:
-            nmsd = [nmsd[i]+t for i,t in enumerate(v['msd'])] if len(nmsd) > 0 else [t for i,t in enumerate(v['msd'])]
-    except:
-        print('something went wrong with msd')
-
-try:
-    if len(bmsd) > 0:
-        bmsd = [t/float(len(bmsd)) for t in bmsd]
-    if len(nmsd) > 0:
-        nmsd = [t/float(len(nmsd)) for t in nmsd]
-except:
-    print('couldnt get msd avg')
-
-print bmsd
-print nmsd
 
 df = pd.DataFrame(data, columns = ['file','den','cls','bud', 'cat' ]) 
 df.to_csv(os.path.join(outPath,'trajectories-summary.csv'),index=False)
