68d3bb03797181bfc329b37ccc965fe62c8b3dd3
Joel Forster <forsterjoel@gmail.com>
2020-01-23 19:34:15 +0000

    new figures

--- runsinglefile.py
+++ runsinglefile.py
@@ -15,7 +15,7 @@ parser.add_argument('-o','--out', default='', type=str,
 
 args = parser.parse_args()
 
-istr = "010000011000100010101000101000001100000010010111100001101001110000010000"
+istr = "010000001000100000010101001100001110000010001010001011100010000111001100"
 individual = [int(i) for i in istr]
 
 particle = nanoparticle.CoveredNanoParticlePhenome(individual,1,0,10,10)
@@ -25,14 +25,14 @@ np = particle.particle
 sim = mb.MembraneSimulation(
         args.input.split('/')[-1].split('.')[0],
         np,
-        25000,
+        50000,
         0.01,        
         args.out,
         os.path.dirname(args.input),
         "/Users/joelforster/Projects/optihedron/mem/template/data.template",
         "/Users/joelforster/Projects/optihedron/mem/template/in.template",
-        rAxis=(0,1,0),
-        rAmount=0        
+        rAxis=(1,1,0),
+        rAmount=3.141        
         )
 
 sim.saveFiles()
